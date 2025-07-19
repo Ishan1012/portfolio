0dc4c1215bf2868ecf65e3c79f5a7d3246c1b6ba
Ishan1012 <ishandwivedi1210@gmail.com>
2025-07-19 16:58:31 +0530

    Remove boot sequence logic and LoadingPage component from Home

--- app/page.js
+++ app/page.js
@@ -4,24 +4,18 @@ import Taskbar from '@/components/Taskbar'
 import React, { useState } from 'react'
 
 import dynamic from 'next/dynamic';
-import LoadingPage from '@/components/LoadingPage';
 
 const Clock = dynamic(() => import('../components/Clock'), { ssr: false });
 
 export default function Home() {
-  const [bootComplete, setBootComplete] = useState(false);
 
   return (
     <div className='loading'>
-      {bootComplete ? (
-        <div className='container'>
+      <div className='container'>
         <Background />
         <Taskbar />
         <Clock />
       </div>
-      ) : (
-        <LoadingPage onComplete={() => setBootComplete(true)} />
-      )}
     </div>
   )
 }
