d3945db475f2ed50fd4e59e80d94587a6d9bc478
Ishan1012 <ishandwivedi1210@gmail.com>
2025-08-05 00:35:56 +0530

    fix: minor bugs fixed

--- components/TaskCollection/FolderExplorer.js
+++ components/TaskCollection/FolderExplorer.js
@@ -1,25 +1,45 @@
-import React from 'react'
-import '../styles/FolderExplorer.css'
+'use client';
+import React, { useState } from 'react';
+import '../styles/FolderExplorer.css';
+import Image from 'next/image';
 
-export default function FolderExplorer({ tasks }) {
+export default function FolderExplorer() {
+  const [clicked, setClicked] = useState(null);
+
+  const frameWidth = 80;
+  const frameHeight = 80;
+
+  const tasks = [
+    { image: 'chrome.png' },
+    { image: 'chrome.png' },
+    { image: 'chrome.png' },
+    { image: 'chrome.png' },
+    { image: 'chrome.png' },
+  ];
+
+  const handleClick = (index) => {
+    setClicked(index);
+    setTimeout(() => {
+      setClicked(null);
+    }, 150);
+  };
+
   return (
-    <div className='settingsContainer'>
-      <h1 className='title'>Background Settings</h1>
+    <div className='folderContainer'>
+      <h1 className='title'>Folder Explorer</h1>
       <div className="imageContainer">
-        {
-          tasks.map((item, index) => (
-            <Image
-              key={index}
-              className={`image ${clicked === index ? 'clicked' : ''}`}
-              src={"/background/" + item.image}
-              width={frameWidth}
-              height={frameHeight}
-              alt={'background' + index}
-              onClick={() => handleClick(index)}
-            />
-          ))
-        }
+        {tasks.map((item, index) => (
+          <Image
+            key={index}
+            className={`image ${clicked === index ? 'clicked' : ''}`}
+            src={`/icons/${item.image}`} 
+            width={frameWidth}
+            height={frameHeight}
+            alt={`item-${index}`}
+            onClick={() => handleClick(index)}
+          />
+        ))}
       </div>
     </div>
-  )
+  );
 }
